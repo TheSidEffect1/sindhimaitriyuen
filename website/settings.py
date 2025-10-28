195e53565afe88301bed360242503813583107a8
TheSidEffect1 <siddharthkodwani33@gmail.com>
2025-10-28 23:40:49 +0530

    Fix static path for Render

--- website/settings.py
+++ website/settings.py
@@ -15,10 +15,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
