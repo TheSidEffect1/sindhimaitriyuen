bc0fe90f30a94dc3d63639b8f5bc3c9ed1e01032
TheSidEffect1 <siddharthkodwani33@gmail.com>
2025-10-28 23:17:50 +0530

    update allowed hosts

--- website/settings.py
+++ website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-!e*7asa9lcm&dc&gia%qg$d6%khcz-f1k(8fe=$ax%a^gdrf4n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com', 'sindhimaitryuen.com', 'www.sindhimaitryuen.com']
+ALLOWED_HOSTS = ['.onrender.com', 'sindhimaitryuen.com', 'www.sindhimaitryuen.com', 'sindhimaitriyuen.onrender.com']
 
 
 # Application definition
